c1163554eba89821276ccc58c20096faa294250d
Pierre-Emmanuel Lévesque <pierre.e.levesque@gmail.com>
2019-05-24 19:15:10 -0400

    changed a variable name

--- index.js
+++ index.js
@@ -3,12 +3,12 @@
 const substrOccurrence = require('substr-occurrence')
 
 module.exports = (str, checks) => {
-  let result = true
+  let haveMinimumOccurences = true
   for (let i = 0; i < checks.length; i++) {
     if (substrOccurrence(checks[i][0], str) < checks[i][1]) {
-      result = false
+      haveMinimumOccurences = false
       break
     }
   }
-  return result
+  return haveMinimumOccurences
 }
